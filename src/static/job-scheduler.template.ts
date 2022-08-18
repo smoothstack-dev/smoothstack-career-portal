50dc4aa6fec0889d25610814997fc3f017c44011
derekhcchou <derekchc53@gmail.com>
2022-08-17 22:10:21 -0700

    css and typo fix

--- src/static/job-scheduler.template.ts
+++ src/static/job-scheduler.template.ts
@@ -7,7 +7,7 @@ export const defaultChallengeInfo = {
     {
       contentType: 'LIST',
       contents: [
-        { title: 'Question 1:', content: 'Choodse your Own language' },
+        { title: 'Question 1:', content: 'Choose your Own language' },
         { title: 'Question 2:', content: 'General Command Language (Bash)' },
         { title: 'Question 3:', content: 'SQL' },
         { title: 'Question 4-13:', content: 'Multiple Choice' },
